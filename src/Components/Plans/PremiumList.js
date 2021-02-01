85078b6cb4601a2697e5c72fee1a3c6517589db8
sreejith <sreejith.puthenveettil@cleartrip.com>
2021-02-01 14:30:37 +0530

    modified responnsive componnennnts

--- src/Components/Plans/PremiumList.js
+++ src/Components/Plans/PremiumList.js
@@ -4,6 +4,13 @@ import { ReactComponent as Spelling } from "../../assets/images/spelling.svg";
 import { ReactComponent as Grammer } from "../../assets/images/grammer.svg";
 import { ReactComponent as Punctuation } from "../../assets/images/punctuation.svg";
 import { ReactComponent as Conciseness } from "../../assets/images/conciseness.svg";
+
+import { ReactComponent as PremiumOne } from "../../assets/images/premiumOne.svg";
+import { ReactComponent as PremiumTwo } from "../../assets/images/premiumTwo.svg";
+import { ReactComponent as PremiumThree } from "../../assets/images/premiumThree.svg";
+import { ReactComponent as PremiumFour } from "../../assets/images/premiumFour.svg";
+import { ReactComponent as PremiumFive } from "../../assets/images/premiumFive.svg";
+
 function PremiumList(props) {
     return (
         <>
@@ -15,55 +22,50 @@ function PremiumList(props) {
           />
           <ListItem
             copy="Clarity-focused sentence rewrites"
-            modelImage={<Grammer />}
+            modelImage={<PremiumOne />}
             isModel={true}
-            modelCopy="Eliminate spelling errors."
+            modelCopy="Automatically rewrite hard-to-read sentences."
           />
           <ListItem
             copy="Tone adjustments"
             isModel={true}
-            modelImage={<Punctuation />}
+            modelImage={<PremiumTwo />}
             modelCopy="Eliminate spelling errors."
           />
           <ListItem
             copy="Plagiarism detection"
             isModel={true}
-            modelImage={<Conciseness />}
             modelCopy="Eliminate spelling errors."
           />
-        <ListItem
-            copy="Plagiarism detection"
-            isModel={true}
-            modelImage={<Conciseness />}
-            modelCopy="Eliminate spelling errors."
-          />
-
         <ListItem
             copy="Word choice"
             isModel={true}
-            modelImage={<Conciseness />}
+            modelImage={<PremiumThree />}
             modelCopy="Eliminate spelling errors."
           />
 
         <ListItem
             copy="Formality level"
             isModel={true}
-            modelImage={<Conciseness />}
-            modelCopy="Eliminate spelling errors."
+            modelImage={<PremiumFour />}
+            modelCopy="Write with the appropriate tone, even when you're in a hurry."
           />
+
 
         <ListItem
             copy="Fluency"
             isModel={true}
-            modelImage={<Conciseness />}
-            modelCopy="Eliminate spelling errors."
+            modelImage={<PremiumFive />}
+            modelCopy="Ensure your word choices sound natural and fluent."
           />
-          <ListItem
+
+
+        <ListItem
             copy="Additional advanced suggestions"
             isModel={true}
-            modelImage={<Conciseness />}
-            modelCopy="Additional advanced suggestions"
+            modelCopy="Fix inconsistencies in spelling and punctuation, adjust the tone of your writing, and get additional advanced feedback."
           />
+
           
         </>
     );
